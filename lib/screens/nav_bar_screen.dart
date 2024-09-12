fa58c1f119053f2a8cbd8aa37a134ad32274c09e
ishuravi <Iswarya3202@gmail.com>
2024-09-12 21:17:40 +0530

    first commit

---
+++ lib/screens/nav_bar_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/constants.dart';
+import 'package:shopping/screens/Cart/cart_screen.dart';
+import 'package:shopping/screens/Home/home_screen.dart';
+import 'package:shopping/screens/Profile/profile.dart';
+import 'package:shopping/screens/Favorite/favorite.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int currentIndex = 2;
+  late List<Widget> screens;
+
+  @override
+  void initState() {
+    super.initState();
+    screens = [
+      Scaffold(),
+      Favorite(),
+      HomeScreen(),
+      CartScreen(),
+      Profile(onTabSelected: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      }),
+    ];
+  }
+
+  void setIndex(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setIndex(2);
+        },
+        shape: const CircleBorder(),
+        backgroundColor: kprimaryColor,
+        child: const Icon(
+          Icons.home,
+          color: Colors.white,
+          size: 35,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        elevation: 1,
+        height: 60,
+        color: Colors.white,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 10,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                setIndex(0);
+              },
+              icon: Icon(
+                Icons.grid_view_outlined,
+                size: 30,
+                color: currentIndex == 0 ? kprimaryColor : Colors.grey.shade400,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                setIndex(1);
+              },
+              icon: Icon(
+                Icons.favorite_border,
+                size: 30,
+                color: currentIndex == 1 ? kprimaryColor : Colors.grey.shade400,
+              ),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            IconButton(
+              onPressed: () {
+                setIndex(3);
+              },
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+                size: 30,
+                color: currentIndex == 3 ? kprimaryColor : Colors.grey.shade400,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                setIndex(4);
+              },
+              icon: Icon(
+                Icons.person,
+                size: 30,
+                color: currentIndex == 4 ? kprimaryColor : Colors.grey.shade400,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: screens[currentIndex],
+    );
+  }
+}
